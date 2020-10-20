34e221db6301651b6969820703efce9d1e66056a
Kevin Fontela <fontela98@yahoo.com>
2020-10-19 23:15:34 -0400

    Create Pages and components Folders

---
+++ portfolio/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class HomePage extends React.Component
+{
+
+
+    render() {
+        return (
+            <div>
+                <h1>this will be the resume link section</h1>
+
+                <h2>this will be the resume section</h2>
+
+                <h3>this will be the footer section</h3>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
