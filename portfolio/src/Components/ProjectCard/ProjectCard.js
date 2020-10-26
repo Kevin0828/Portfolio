5a79c25ec4fc5238307df598bef1546acdd65035
kevin fontela <fontela98@yahoo.com>
2020-10-26 14:30:16 -0400

    Fixed json file with data and fixed cards to receive data from json file

--- portfolio/src/Components/ProjectCard/ProjectCard.js
+++ portfolio/src/Components/ProjectCard/ProjectCard.js
@@ -10,21 +10,36 @@ class ProjectCard extends React.Component {
                 <Card className="project-card-individual fade-in-fast">
                     <div className = "image-holder-animation">
                         <figure>
-                            <Card.Img variant="top" src={this.props.logo} />
+                            <Card.Img variant="top" src={this.props.cardLogo} />
                         </figure>
                     </div>
                     <Card.Body>
 
                         <Card.Title>{this.props.title}</Card.Title>
 
+                        <Card.Subtitle>
+                            {this.props.subTitle}
+                        </Card.Subtitle>
+
+                        <hr></hr>
+
                         <Card.Text>
                             {this.props.text}
                         </Card.Text>
 
+                        <Card.Text>
+                            {this.props.skill_1}
+                            {this.props.skill_2}
+                            {this.props.skill_3}
+                        </Card.Text>
+
+                        
+
+
                         <div className="card-links">
-                            <a href={this.props.linkedIn_link}><img className="card-project-links" src={this.props.linkedIn} /></a>
-                            <a href={this.props.viewWeb_link}><img className="card-project-links" src={this.props.viewWeb} /></a>
-                            <a href={this.props.video_link}><img className="card-project-links" src={this.props.video} /></a>
+                            <a target="_blank" href={this.props.link_1}><img alt="" className="card-project-links" src={this.props.image_1} /></a>
+                            <a href={this.props.link_2}><img alt="" className="card-project-links" src={this.props.image_2} /></a>
+                            <a href={this.props.link_3}><img alt="" className="card-project-links" src={this.props.image_3} /></a>
                         </div>
                     </Card.Body>
                 </Card>
