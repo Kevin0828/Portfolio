4f1094a14da43a2c8f3ad876e190847023153b79
Kevin Fontela <fontela98@yahoo.com>
2020-11-04 12:36:58 -0500

    updates

--- portfolio/src/Components/ContactMe/ContactMe.js
+++ portfolio/src/Components/ContactMe/ContactMe.js
@@ -3,23 +3,23 @@ import './ContactMe.css';
 import { Button } from 'react-bootstrap';
 
 
-
 class ContactMe extends React.Component {
     render() {
         return (
             <section className="contact-me-section">
-                <form className ="contact-form">
+                <form  className ="contact-form" action="https://mailthis.to/contactkevin"
+    method="POST" encType="multipart/form-data">
                     <div className="contact-name">
                         <input className="contact-input" name="name" placeholder="Enter Name" required/>
                     </div>
                     <div className="contact-name">
-                        <input type = "email" className="contact-input" name="nmail" placeholder="Enter Email" required/>
+                        <input type = "email" className="contact-input" name="_replyto" placeholder="Enter Email" required/>
                     </div>
                     <div className="contact-name">
                         <textarea className="contact-input message-input" name="message" placeholder="Your Message" minLength = "15" maxLength="500" required/>
                     </div>
                     <div className="contact-input-send">
-                        <Button className="form-button" variant="outline-success" type="submit">Submit</Button>
+                        <Button className="form-button" variant="outline-success" type="submit" >Submit</Button>
                     </div>
                 </form>
             </section>
